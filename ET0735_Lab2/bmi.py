3bd410e0b2f9bb62f6fa1513714b89384b8784e6
Jaedon <JAEDONLAU68.22@ichat.sp.edu.sg>
2023-05-04 15:11:28 +0800

    edited

--- ET0735_Lab2/bmi.py
+++ ET0735_Lab2/bmi.py
@@ -1,17 +1,18 @@
 def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
-
+    checking = 0
     BMI = weight/(height*height)
     print("BMI = ", BMI)
     if (BMI<18.5):
-        print("-1")
+        checking = -1
     if (BMI>25):
-        print("1")
+        checking = 1
 
     else :
-        print("0")
-
+        checking = 0
+    print(checking)
+    return checking
 
 
 calculate_bmi(weight=57, height=1.73)
